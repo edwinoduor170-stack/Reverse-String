ce754d84b03ef9ead62035344e6d9052963242f6
Edwin Oduor <edwinoduor170@gmail.com>
2025-09-27 12:16:39 +0300

    Add files via upload

---
+++ edwinOduor.js
@@ -0,0 +1,14 @@
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+console.log(reverseString("hello"));
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+console.log(reverseString("12345"));
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+console.log(reverseString(""));
